c3ef58017a3f21ac6004a8d1cdf42c1fe059950e
Zainab Ashiq <misszainabashiq@gmail.com>
2026-07-22 17:35:26 +0500

    up

--- resources/views/components/user_sidebar2.blade.php
+++ resources/views/components/user_sidebar2.blade.php
@@ -358,6 +358,7 @@
             @endif
             @if(auth()->check() && auth()->user()->product_freezing == 1)
                         <li class="side-nav-item">
+                            <i class="uil-box"></i>
                             <a href="{{ route('product-freezing.index') }}">Product Freezing</a>
                         </li>
             @endif
